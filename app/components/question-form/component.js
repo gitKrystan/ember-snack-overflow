670e4ac4ae3b2dc9686b6a3135983f473f48ac8e
Krystan Menne <kmenne@gmail.com>
2016-02-26 11:00:56 -0800

    add route to view list of questions

--- app/components/question-form/component.js
+++ app/components/question-form/component.js
@@ -30,7 +30,7 @@ export default Ember.Component.extend({
           notes: this.get('notes') || '',
           dateCreated: new Date().getTime()
         };
-        
+
         this.set('author', '');
         this.set('content', '');
         this.set('notes', '');
--- app/index/route.js
+++ app/index/route.js
@@ -1,10 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('question', {reload: true})
+      .then(function(questions) {
+        return questions.sortBy('date').reverse();
+      }
+    );
+  },
+
   actions: {
     saveQuestion(params) {
+      var route = this;
       var newQuestion = this.store.createRecord('question', params);
-      newQuestion.save();
+      newQuestion.save().then(function() {
+        route.refresh();
+      });
     }
   }
 });
