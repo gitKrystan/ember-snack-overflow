c8714d96d5291db91bac7eec2e9fa8a4b67ff2bb
Krystan Menne <kmenne@gmail.com>
2016-02-26 09:15:35 -0800

    add question model

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'snack-overflow',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://snack-overflow.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
