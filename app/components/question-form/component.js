2f8cc8f6cf7cd346c252cc329249a856aeda8ee4
Krystan Menne <kmenne@gmail.com>
2016-02-26 10:45:11 -0800

    add route to add new questions

---
+++ app/components/question-form/component.js
@@ -0,0 +1,42 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'form',
+  classNames: ['form-horizontal'],
+  warnNeedsAuthor: false,
+  warnNeedsContent: false,
+  stopSubmit: false,
+  actions: {
+    saveQuestion() {
+      var inputAuthor = this.get('author');
+      var inputContent = this.get('content');
+
+      if (!inputAuthor) {
+        this.set('warnNeedsAuthor', true);
+        this.set('stopSubmit', true);
+      }
+
+      if (!inputContent) {
+        this.set('warnNeedsContent', true);
+        this.set('stopSubmit', true);
+      }
+
+      if (this.get('stopSubmit')) {
+        return this.set('stopSubmit', false);
+      } else {
+        var params = {
+          author: inputAuthor,
+          content: inputContent,
+          notes: this.get('notes') || '',
+          dateCreated: new Date().getTime()
+        };
+        
+        this.set('author', '');
+        this.set('content', '');
+        this.set('notes', '');
+
+        this.sendAction('saveQuestion', params);
+      }
+    }
+  }
+});
--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+    }
+  }
 });
